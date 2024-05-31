66d621d2e7a6da5e3b83884d666ba1f69c2c37ac
zgoreck2 <01150958@pw.edu.pl>
2024-05-31 20:12:57 +0200

    Change outlier factors params from List[np.ndarray] to np.ndarray

--- algorithm/NNAnomalyDetector.py
+++ algorithm/NNAnomalyDetector.py
@@ -15,38 +15,31 @@ class NNAnomalyDetector:
         self.outlier_factor = outlier_factor
 
         self.fitted = False
-        self.pred_distances = None
-        self.pred_neighbours = None
 
-    def _k_distance(self) -> List[float]:
-        return [distance_row[-1] for distance_row in self.pred_distances]
+    def _k_distance(self, distances: np.ndarray, *argv) -> float:
+        return distances[-1]
     
-    def _mean_knn_distance(self) -> List[float]:
-        return [distance_row.mean() for distance_row in self.pred_distances]
+    def _mean_knn_distance(self, distances: np.ndarray, *argv) -> float:
+        return distances.mean()
     
-    def _loc_reachability_density(self) -> List[float]:
-        loc_reachability_density_list = []
-        for example_pred_distances, example_pred_neighbours in zip(self.pred_distances, self.pred_neighbours):
-            max_nn_dist = example_pred_distances[-1]
-            reachability_sum = 0
-            for distance, neighbour in zip(example_pred_distances, example_pred_neighbours):
-                neigh_dist, _ =  self.kNN.predict(self.kNN.X_train[neighbour].reshape(1, -1))
-                reachability_sum += max(max_nn_dist, neigh_dist[0][-1])
-            loc_reachability_density_list.append(1/(reachability_sum/len(example_pred_neighbours)))
-        return loc_reachability_density_list
+    def _reachability(self, distance: np.ndarray, neighbour_idx: int) -> float:
+        neigh_dist, _ =  self.kNN.predict(self.kNN.X_train[neighbour_idx].reshape(1, -1))
+        return max(distance, self._k_distance(neigh_dist[0]))
+    
+    def _loc_reachability_density(self, distances: np.ndarray, neighbours_idx: np.ndarray) -> float:
+        reachability_sum = 0
+        for distance, neighbour_idx in zip(distances, neighbours_idx):
+            reachability_sum += self._reachability(distance, neighbour_idx)
+        return 1/(reachability_sum/len(neighbours_idx))
 
-    def _lof(self) -> List[float]:
-        lof_list = []
-        for example_pred_distances, example_pred_neighbours in zip(self.pred_distances, self.pred_neighbours):
-            print(f"example_self.pred_distances: {example_pred_distances}")
-            sum = 0
-            for distance, neighbour in zip(example_pred_distances, example_pred_neighbours):
-                neigh_dist, _ =  self.kNN.predict(self.kNN.X_train[neighbour].reshape(1, -1))
-                print(f"neigh_dist: {neigh_dist}")
-                sum += neigh_dist[0][-1]/example_pred_distances[-1]
-                print(f"sum: {sum}")
-            lof_list.append(sum/len(example_pred_neighbours))
-        return lof_list
+    def _lof(self, distances: np.ndarray, neighbours_idx: np.ndarray) -> float:
+        sum = 0
+        for distance, neighbour_idx in zip(distances, neighbours_idx):
+            neigh_distances, neigh_neighbours_idx =  self.kNN.predict(self.kNN.X_train[neighbour_idx].reshape(1, -1))
+            print(f"neigh_distances: {neigh_distances}")
+            sum += self._loc_reachability_density(neigh_distances[0], neigh_neighbours_idx[0])
+            print(f"sum: {sum}")
+        return sum/self._loc_reachability_density(distances, neighbours_idx)/len(neighbours_idx)
 
     def fit(self, X: np.ndarray) -> None:
         self.kNN = KNN(self.k, self.metric)
@@ -57,9 +50,18 @@ class NNAnomalyDetector:
     def predict(self, X: np.ndarray, thresh: float = None) -> np.ndarray | tuple[np.ndarray, np.ndarray]:
         if not self.fitted:
             raise ValueError("Model has not been trained yet")
-        
-        self.pred_distances, self.pred_neighbours = self.kNN.predict(X)
-        outlier_factor_list = self.outlier_factor()
+           
+        distances, neighbours_idx = self.kNN.predict(X)
+        print('distances')
+        print(distances)
+        print('neighbours_idx')
+        print(neighbours_idx)
+
+        outlier_factor_list = []
+        for example_distances, example_neighbours_idx in zip(distances, neighbours_idx):
+            example_outlier_factor = self.outlier_factor(example_distances, example_neighbours_idx)
+            outlier_factor_list.append(example_outlier_factor)
+
         if thresh is not None:
             return outlier_factor_list, (np.array(outlier_factor_list) > thresh).astype(int)
         else:
