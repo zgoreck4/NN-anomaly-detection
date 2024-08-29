cfd26e8ce4a02c90d9a276d838f74883d825ccd6
zgoreck4 <zgorecka@o2.pl>
2024-08-29 11:51:09 +0200

    add code documentation

--- algorithm/KNN.py
+++ algorithm/KNN.py
@@ -3,6 +3,33 @@ from sklearn.metrics.pairwise import distance_metrics
 from typing import Callable, List, Tuple
 
 class KNN:
+    """
+    A class that implements the k-Nearest Neighbors (kNN) algorithm.
+    In the case where the k-th neighbor is as distant as the k+n-th neighbor the algorithm may return
+    more than k nearest neighbors.
+
+    Parameters:
+    ----------
+    k : int
+        The number of nearest neighbors to consider.
+    metric : str or Callable
+        The distance metric to use. Can be either a string specifying one of the metrics from
+        `sklearn.metrics.pairwise.distance_metrics()` or a custom callable function.
+
+    Attributes:
+    ----------
+    k : int
+        The number of nearest neighbors.
+    metric : str or Callable
+        The distance metric function.
+    X_train : np.ndarray
+        The training dataset.
+    train_distances : List[np.ndarray]
+        A list of distance vectors to the k nearest neighbors for each training data point.
+    train_neigh_idx : List[np.ndarray]
+        A list of index vectors representing the k nearest neighbors for each training data point.
+    """
+
     def __init__(self, k: int, metric: str | Callable):
         self.k = k
         if type(metric)==str:
@@ -19,6 +46,21 @@ class KNN:
         self.train_neigh_idx = None
 
     def _compute_distances(self, x1: np.ndarray, exclude_index: int=None) -> List[Tuple[float, int]]:
+        """
+        Computes the distances from a given point `x1` to all points in the training set.
+
+        Parameters:
+        ----------
+        x1 : np.ndarray
+            The point from which distances are calculated.
+        exclude_index : int, optional
+            The index of a point in `X_train` to exclude from the distance calculation. Useful during training.
+
+        Returns:
+        ----------
+        List[Tuple[float, int]]
+            A list of tuples containing the distance and the index of the corresponding training point.
+        """
         dist_idx = []
         for j, x2 in enumerate(self.X_train):
             if exclude_index==j:
@@ -27,6 +69,19 @@ class KNN:
         return dist_idx
     
     def _choose_kNN(self, dist_idx: List[Tuple[float, int]]) -> Tuple[np.ndarray, np.ndarray]: # ->???
+        """
+        Selects the k nearest neighbors from a list of distances.
+
+        Parameters:
+        ----------
+        dist_idx : List[Tuple[float, int]]
+            A list of tuples containing distances and indices of points in the training set.
+
+        Returns:
+        ----------
+        Tuple[np.ndarray, np.ndarray]
+            A tuple containing two arrays: the distances of the k nearest neighbors and their indices.
+        """
         dist_idx.sort(key=lambda x: x[0])
         neighbours_dist = []
         neighbours_idx = []
@@ -41,6 +96,14 @@ class KNN:
         return np.array(neighbours_dist), np.array(neighbours_idx)
     
     def _fit_predict(self, X: np.ndarray) -> None:
+        """
+        Fits the model to the training data and predicts the nearest neighbors for each training point.
+
+        Parameters:
+        ----------
+        X : np.ndarray
+            The training dataset.
+        """
         neighbours_dist_list = []
         neighbours_idx_list = []
         for i, x in enumerate(X):
@@ -52,10 +115,33 @@ class KNN:
         self.train_neigh_idx = neighbours_idx_list
         
     def fit(self, X_train: np.ndarray) -> None:
+        """
+        Trains the kNN model using the provided training dataset.
+
+        Parameters:
+        ----------
+        X_train : np.ndarray
+            The training dataset.
+        """
         self.X_train = X_train
         self._fit_predict(X_train)
     
     def predict(self, X: np.ndarray) -> tuple[List[np.ndarray], List[np.ndarray]]:
+        """
+        Predicts the k nearest neighbors for each point in the input data.
+
+        Parameters:
+        ----------
+        X : np.ndarray
+            The input data for which to predict the nearest neighbors.
+
+        Returns:
+        ----------
+        tuple[List[np.ndarray], List[np.ndarray]]
+            A tuple containing two lists:
+            - `neighbours_dist_list`: A list of arrays, each containing the distances to the k nearest neighbors.
+            - `neighbours_idx_list`: A list of arrays, each containing the indices of the k nearest neighbors.
+        """
         if len(self.X_train)==0:
             raise ValueError("Model has not been trained yet")
         neighbours_dist_list = []
@@ -68,5 +154,20 @@ class KNN:
         return neighbours_dist_list, neighbours_idx_list
     
     def fit_predict(self, X: np.ndarray) -> tuple[List[np.ndarray], List[np.ndarray]]:
+        """
+        Trains the kNN model on the provided data and returns the k nearest neighbors for each point in the training data.
+
+        Parameters:
+        ----------
+        X : np.ndarray
+            The training dataset.
+
+        Returns:
+        ----------
+        tuple[List[np.ndarray], List[np.ndarray]]
+            A tuple containing two lists:
+            - `neighbours_dist_list`: A list of arrays, each containing the distances to the k nearest neighbors.
+            - `neighbours_idx_list`: A list of arrays, each containing the indices of the k nearest neighbors.
+        """
         self.fit(X)
         return self.train_distances, self.train_neigh_idx
--- algorithm/NNAnomalyDetector.py
+++ algorithm/NNAnomalyDetector.py
@@ -4,11 +4,49 @@ from typing import Callable, List, Union
 from KNN import KNN
 
 class NNAnomalyDetector:
+    """
+    A class for detecting anomalies using k-Nearest Neighbors (kNN) based methods.
+
+    Parameters:
+    ----------
+    k : int
+        The number of nearest neighbors to consider.
+    metric : str or Callable
+        The distance metric to use. Can be either a string specifying one of the metrics from
+        `sklearn.metrics.pairwise.distance_metrics()` or a custom callable function.
+    outlier_factor_input : str or Callable
+        The method used to calculate the outlier factor. Can be either a custom function or one of the
+        following predefined methods:
+        - 'k_distance': Outlier factor based on the distance to the k-th nearest neighbor.
+        - 'mean_knn_distance': Outlier factor based on the mean distance to the k nearest neighbors.
+        - 'negative_loc_reachability_density': Negative local reachability density.
+        - 'lof': Local Outlier Factor (LOF).
+    
+    Attributes:
+    ----------
+    k : int
+        The number of nearest neighbors.
+    metric : Callable
+        The distance metric function.
+    outlier_factor : Callable
+        The function used to calculate the outlier factor.
+    fitted : bool
+        Indicates whether the model has been trained.
+    kNN : KNN
+        The trained kNN model.
+    """
+
     def __init__(self, k: int, metric: str | Callable, outlier_factor_input: str | Callable):
         self.k = k
         self.metric = metric
 
-        outlier_factor_dict = {'k_distance': self._k_distance, 'mean_knn_distance': self._mean_knn_distance, "negative_loc_reachability_density": self._negative_loc_reachability_density, "lof": self._lof}
+        # Dictionary mapping outlier factor names to methods
+        outlier_factor_dict = {
+            'k_distance': self._k_distance,
+            'mean_knn_distance': self._mean_knn_distance,
+            "negative_loc_reachability_density": self._negative_loc_reachability_density,
+            "lof": self._lof
+        }
         outlier_factor = outlier_factor_dict.get(outlier_factor_input)
         if outlier_factor==None:
             raise ValueError("Invalid outlier factor metric name")
@@ -17,35 +55,133 @@ class NNAnomalyDetector:
         self.fitted = False
 
     def _k_distance(self, distances: np.ndarray, *argv) -> float:
+        """
+        Calculates the k-distance, which is the distance to the k-th nearest neighbor.
+
+        Parameters:
+        ----------
+        distances : np.ndarray
+            Array of distances to neighbors.
+
+        Returns:
+        ----------
+        float
+            The distance to the k-th nearest neighbor.
+        """
         return distances[-1]
     
     def _mean_knn_distance(self, distances: np.ndarray, *argv) -> float:
+        """
+        Calculates the mean distance to the k nearest neighbors.
+
+        Parameters:
+        ----------
+        distances : np.ndarray
+            Array of distances to neighbors.
+
+        Returns:
+        ----------
+        float
+            The mean distance to the k nearest neighbors.
+        """
         return distances.mean()
     
     def _reachability(self, distance: np.ndarray, neighbour_idx: int) -> float:
+        """
+        Calculates the reachability distance between a point and its neighbor.
+
+        Parameters:
+        ----------
+        distance : np.ndarray
+            The distance to the neighbor.
+        neighbour_idx : int
+            The index of the neighbor.
+
+        Returns:
+        ----------
+        float
+            The reachability distance, which is the maximum of the distance to the neighbor and its k-distance.
+        """
         neigh_dist = self.kNN.train_distances[neighbour_idx]
         return max(distance, self._k_distance(neigh_dist))
     
     def _negative_loc_reachability_density(self, distances: np.ndarray, neighbours_idx: np.ndarray) -> float:
+        """
+        Calculates the negative local reachability density (LRD).
+
+        Parameters:
+        ----------
+        distances : np.ndarray
+            Array of distances to neighbors.
+        neighbours_idx : np.ndarray
+            Array of neighbor indices.
+
+        Returns:
+        ----------
+        float
+            The negative local reachability density.
+        """
         reachability_sum = 0
         for distance, neighbour_idx in zip(distances, neighbours_idx):
             reachability_sum += self._reachability(distance, neighbour_idx)
         return -1/(reachability_sum/len(neighbours_idx))
 
     def _lof(self, distances: np.ndarray, neighbours_idx: np.ndarray) -> float:
-        sum = 0
+        """
+        Calculates the Local Outlier Factor (LOF) for a point.
+
+        Parameters:
+        ----------
+        distances : np.ndarray
+            Array of distances to neighbors.
+        neighbours_idx : np.ndarray
+            Array of neighbor indices.
+
+        Returns:
+        ----------
+        float
+            The Local Outlier Factor.
+        """
+        sum_lof = 0
         for distance, neighbour_idx in zip(distances, neighbours_idx):
             neigh_distances = self.kNN.train_distances[neighbour_idx]
             neigh_neighbours_idx = self.kNN.train_neigh_idx[neighbour_idx]
-            sum += self._negative_loc_reachability_density(neigh_distances, neigh_neighbours_idx)
-        return sum/self._negative_loc_reachability_density(distances, neighbours_idx)/len(neighbours_idx)
+            sum_lof += self._negative_loc_reachability_density(neigh_distances, neigh_neighbours_idx)
+        return sum_lof/self._negative_loc_reachability_density(distances, neighbours_idx)/len(neighbours_idx)
 
     def fit(self, X: np.ndarray) -> None:
+        """
+        Trains the NNAnomalyDetector model on the provided dataset.
+
+        Parameters:
+        ----------
+        X : np.ndarray
+            The training dataset.
+        """
         self.kNN = KNN(self.k, self.metric)
         self.kNN.fit(X)
         self.fitted = True
     
     def predict(self, X: np.ndarray, thresh: float = None) -> np.ndarray | tuple[np.ndarray, np.ndarray]:
+        """
+        Predicts anomalies in the given dataset.
+
+        Parameters:
+        ----------
+        X : np.ndarray
+            The dataset to classify.
+        thresh : float, optional
+            A threshold value for classifying a point as an anomaly. If provided, the method returns
+            both the outlier factor and the binary classification (1 for anomaly, 0 for normal).
+            If not provided, only the outlier factor is returned.
+
+        Returns:
+        ----------
+        Union[np.ndarray, tuple[np.ndarray, np.ndarray]]
+            - If `thresh` is None: Returns an array of outlier factors.
+            - If `thresh` is provided: Returns a tuple where the first element is the array of outlier factors,
+              and the second element is a binary array indicating whether each point is an anomaly.
+        """
         if not self.fitted:
             raise ValueError("Model has not been trained yet")
            
